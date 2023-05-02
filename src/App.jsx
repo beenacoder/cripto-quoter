2345827dcdada3320d98ba3bc950c359f7ffc2b2
Ariel Cabeza <cervanteslibch@gmail.com>
2023-05-02 00:46:46 -0300

    Agregue cotizacion dinamica

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,56 @@
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled"; 
 import ImagenCripto from './assets/img/imagen-criptos.png'
 import Formulario from "./components/Formulario";
+
+
+
+
+function App() {
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+
+  useEffect(() => {
+    //Prevenimos la primera ejecucion con un if Object.keys...
+    if(Object.keys(monedas).length > 0) {
+      const cotizarCripto = async () =>{
+        const {moneda, monedaCripto} = monedas;
+
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${monedaCripto}&tsyms=${moneda}`
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setResultado(resultado.DISPLAY[monedaCripto][moneda]);
+      }
+
+      cotizarCripto();
+
+    }
+    },[monedas])
+
+  return (
+    <Contenedor>
+      <Imagen 
+        src={ImagenCripto}
+        alt="Imagen Cripto Monedas"
+      />
+
+      <div>
+        <Heading>
+          Cotiza Criptomonedas al instante
+        </Heading>
+        <Formulario 
+          setMonedas={setMonedas}
+        />
+      </div>
+    </Contenedor>
+  )
+}
+
+
+
+
+//--------------STYLED COMPONENTS--------------
 
 const Contenedor = styled.div`
   max-width:900px;
@@ -39,26 +89,5 @@ const Heading = styled.h1`
     margin: 10px auto 0 auto;
   }
 `
-
-
-function App() {
-
-
-  return (
-    <Contenedor>
-      <Imagen 
-        src={ImagenCripto}
-        alt="Imagen Cripto Monedas"
-      />
-
-      <div>
-        <Heading>
-          Cotiza Criptomonedas al instante
-        </Heading>
-        <Formulario />
-      </div>
-    </Contenedor>
-  )
-}
 
 export default App
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,84 @@
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from '../data/monedas';
+import Error from "./Error";
+
+
+
+const Formulario = ({setMonedas}) => {
+    const [criptos, setCriptos] = useState([]);
+    const [error, setError] = useState(false);
+
+
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas);
+    const [monedaCripto, SelectMonedasCripto] = useSelectMonedas('Elige tu CriptoMoneda', criptos);
+
+ 
+
+    useEffect(()=>{
+        const consultaApi = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCripto = resultado.Data.map(cripto => {
+
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+
+                //Si no ponemos el return el array nos devuelve undefined
+                return objeto;
+            })
+            setCriptos(arrayCripto);
+        }
+        consultaApi();
+    },[]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if([moneda, monedaCripto].includes('')){
+            setError(true)
+            return 
+        }
+        setError(false);
+        setMonedas({
+            moneda, monedaCripto
+        })
+    }
+
+    return (
+        <>
+            {error && <Error> Todos los campos son obligatorios </Error>} 
+            <form
+                onSubmit={handleSubmit}
+            >
+                <SelectMonedas />
+
+                <SelectMonedasCripto />
+                
+                <InputSubmit 
+                    type="submit" 
+                    value="Cotizar" 
+                />
+            </form>
+        
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+// ---------------STYLED COMPONENTS----------------
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -12,23 +92,15 @@ const InputSubmit = styled.input`
     text-align: center;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7a7dfe;
         cursor: pointer;
     }
-
 `
 
-const Formulario = () => {
-  return (
-    <form>
-        <InputSubmit 
-            type="text" 
-            value="Cotizar" 
-        />
-    </form>
-  )
-}
+
 
 export default Formulario
+
